83d44b14d4a302ec6a6901a781f1176a41e5f6c5
martabalairon <marta.balairon.02@gmail.com>
2024-05-22 10:34:58 +0200

    update codes

--- translate_publications/lang_update.py
+++ translate_publications/lang_update.py
@@ -11,13 +11,13 @@ def detect_language(text):
     except:
         return 'unknown'
 
-INITIAL_RUN = True
-df = pd.read_parquet('data_ingest/match_semanticScholar_researchPortal/match_outputs/publications_translated.parquet')
-
+INITIAL_RUN = False
+#df = pd.read_parquet('/Volumes/data_ml4ds/AI4U/Datasets/ResearchPortal/20240321/processed_parquets/publications_translated.parquet')
+df = pd.read_parquet('/Volumes/usuarios_ml4ds/mbalairon/github/data_ingest/match_semanticScholar_researchPortal/match_outputs/publications_translated.parquet')
 if INITIAL_RUN:
     df['lang'] = 'unknown'
     df['nmt_en'] = None
-    df = df.dropna(subset=['abstract'])
+    df = df.dropna(subset=['title'])
 
-df['lang'] = df.apply(lambda row: detect_language(row['abstract']) if row['lang'] == 'unknown' else row['lang'], axis=1)
-df.to_parquet('data_ingest/match_semanticScholar_researchPortal/match_outputs/publications_translated.parquet')
+df['lang'] = df.apply(lambda row: detect_language(row['title']), axis=1)
+df.to_parquet('/Volumes/data_ml4ds/AI4U/Datasets/ResearchPortal/20240321/processed_parquets/publications_translated.parquet')
--- translate_publications/translate_publications_kumo01.py
+++ translate_publications/translate_publications_kumo01.py
@@ -10,8 +10,8 @@ port = 10000
 url = "http://kumo01:{}/translate".format(port) 
 headers = {'content-type': 'application/json'}
 
-df = pd.read_parquet('data_ingest/match_semanticScholar_researchPortal/match_outputs/publications_translated.parquet')
-
+df = pd.read_parquet('/Volumes/data_ml4ds/AI4U/Datasets/ResearchPortal/20240321/processed_parquets/publications_translated.parquet')
+df['nmt_en'] = None
 # Let us see what are the availables languages and how many documents of each language we have
 languages = df['lang']
 all_lang = Counter(languages)
@@ -31,7 +31,7 @@ for iter, (idx, row) in enumerate(tqdm(en_df.iterrows(), desc="Progreso", unit="
     
     #Carry out translation, only if it is not already available
     if pd.isnull(row["nmt_en"]):
-        text_to_translate = {"text": row['abstract'], "to_lang": "en"}
+        text_to_translate = {"text": row['title'], "to_lang": "en"}
 
 
         #Carry out translation
@@ -57,7 +57,7 @@ for iter, (idx, row) in enumerate(tqdm(en_df.iterrows(), desc="Progreso", unit="
 
     if (iter+1) % 100 == 0:
         #Save after 1000 translations
-        df.to_parquet('data_ingest/match_semanticScholar_researchPortal/match_outputs/publications_translated.parquet', index=False)
+        df.to_parquet('/Volumes/data_ml4ds/AI4U/Datasets/ResearchPortal/20240321/processed_parquets/publications_translated.parquet', index=False)
 
 
-df.to_parquet('data_ingest/match_semanticScholar_researchPortal/match_outputs/publications_translated.parquet', index=False)
+df.to_parquet('/Volumes/data_ml4ds/AI4U/Datasets/ResearchPortal/20240321/processed_parquets/publications_translated.parquet', index=False)
